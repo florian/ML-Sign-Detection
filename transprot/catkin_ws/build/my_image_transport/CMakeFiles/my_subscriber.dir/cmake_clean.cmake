7bf29b2ebebb713182712f48db7eecdf033ff27f
dorian <dorianh1994@gmail.com>
2017-06-13 03:16:10 +0200

    create a workspace, reading pics from frames generated by bag file and converting to OpenCV

---
+++ transprot/catkin_ws/build/my_image_transport/CMakeFiles/my_subscriber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/my_subscriber.dir/src/my_subscriber.cpp.o"
+  "/home/dorian/transprot/catkin_ws/devel/lib/my_image_transport/my_subscriber.pdb"
+  "/home/dorian/transprot/catkin_ws/devel/lib/my_image_transport/my_subscriber"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/my_subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
