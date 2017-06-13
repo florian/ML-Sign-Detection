b05a462f37fa5c03f6a7363a0b389243b1d59de7
dorian <dorianh1994@gmail.com>
2017-06-13 12:45:53 +0200

    now the framesare from the camera

---
+++ fromLaptopCamera/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/dorian/test/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/dorian/test/catkin_ws/devel;/home/dorian/ML-Sign-Detection/transprot/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/dorian/test/catkin_ws/devel;/home/dorian/ML-Sign-Detection/transprot/catkin_ws/devel;/opt/ros/indigo" != "" else []
