305c6b0af85de345a972ee0f842ff4026e1b895d
dorian <dorianh1994@gmail.com>
2017-06-15 14:07:30 +0200

    sliding window in frames from laptop camera

---
+++ test_ws/build/cameraFrames/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dorian/test_ws/src/cameraFrames
+# Build directory: /home/dorian/test_ws/build/cameraFrames
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
