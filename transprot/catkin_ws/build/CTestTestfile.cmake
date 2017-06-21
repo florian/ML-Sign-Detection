f193e22c954d4467f0fd4dc94a1b846e09c69964
dorian <dorianh1994@gmail.com>
2017-06-21 20:38:27 +0200

    delete out-time workspace

--- transprot/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dorian/transprot/catkin_ws/src
-# Build directory: /home/dorian/transprot/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(my_image_transport)
--- transprot/catkin_ws/build/my_image_transport/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dorian/transprot/catkin_ws/src/my_image_transport
-# Build directory: /home/dorian/transprot/catkin_ws/build/my_image_transport
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
