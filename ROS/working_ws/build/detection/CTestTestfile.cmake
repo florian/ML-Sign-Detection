2680b3c79182a4fa1ef4f60d9c24a060fb327430
dorian <dorianh1994@gmail.com>
2017-07-04 18:41:59 +0200

    this ws can work on the cargit add working_ws/!

---
+++ ROS/working_ws/build/detection/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /root/traffic_sign_ws/src/detection
+# Build directory: /root/traffic_sign_ws/build/detection
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(dlib_build)
